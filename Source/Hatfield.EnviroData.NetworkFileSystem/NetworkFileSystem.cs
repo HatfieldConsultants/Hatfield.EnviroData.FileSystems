14d7bdb933fa919c6db8f11bba3f78dc621bf675
Daniel <luzhanxun.daniel@gmail.com>
2015-05-15 12:21:30 -0700

    Add http file system and network drive
    Add support for http file system and related unit tests
    #4
    Add support for network drive file system and related unit tests
    #5

---
+++ Source/Hatfield.EnviroData.NetworkFileSystem/NetworkFileSystem.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net;
+
+namespace Hatfield.EnviroData.FileSystems.NetworkFileSystem
+{
+    public class NetworkFileSystem : IFileSystem
+    {
+        private string _filePath = null;
+        private NetworkCredential _credentials = null;
+
+        public NetworkFileSystem(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public NetworkFileSystem(string filePath, string username, string password, string domain)
+        {
+            _filePath = filePath;
+            _credentials = new NetworkCredential(username, password, domain);
+        }
+
+        public DataFromFileSystem FetchData()
+        {
+            Stream stream;
+
+            if (_credentials != null)
+            {
+                string domain = _credentials.Domain;
+                string username = _credentials.UserName;
+                string password = _credentials.Password;
+                string shareName = Path.GetPathRoot(_filePath);
+
+                NetworkDrive networkDrive = new NetworkDrive();
+                IntPtr impersonationHandle = networkDrive.MapDrive(domain, username, password, shareName);
+                string localDrive = networkDrive.LocalDrive;
+                string localFilePath = localDrive + @"\" + _filePath.Substring(Path.GetPathRoot(_filePath).Length);
+
+                FileStream fileStream = new FileStream(localFilePath, FileMode.Open, FileAccess.Read);
+                stream = new MemoryStream();
+                fileStream.CopyTo(stream);
+                stream.Position = 0;
+
+                networkDrive.UnMapDrive(impersonationHandle);
+            }
+            else
+            {
+                stream = new FileStream(_filePath, FileMode.Open, FileAccess.Read);
+            }
+
+            string fileName = Path.GetFileName(_filePath);
+
+            return new DataFromFileSystem(fileName, stream);
+        }
+
+
+        public IEnumerable<DataFromFileSystem> FetchData(IEnumerable<IFileSystemFilter> filters)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Test/Hatfield.EnviroData.FileSystems.HttpFileSystem.Test/HttpFileSystemTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+
+namespace Hatfield.EnviroData.FileSystems.HttpFileSystem.Test
+{
+    [TestFixture]
+    public class HttpFileSystemTest
+    {
+        [Test]
+        [ExpectedException(typeof(UriFormatException))]
+        public void InvalidUriTest()
+        {
+            var uri = "invalid uri";
+            var httpFileSystem = new HttpFileSystem(uri);
+            var dataFromFileSystem = httpFileSystem.FetchData();
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NullUriTest()
+        {
+            var httpFileSystem = new HttpFileSystem(null);
+            httpFileSystem.FetchData();
+        }
+    }
+}
