6ff7a3f8e1be3450f8ea0a55764b93a64bb09089
zlu <luzhanxun.daniel@gmail.com>
2015-05-14 23:02:40 -0700

    Add FTP file system
    Add FTP file system and related unit tests
    #3

---
+++ Test/Hatfield.EnviroData.FileSystems.FTPSystem.Test/FtpFileSystemTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+using Hatfield.EnviroData.FileSystems.FTPFileSystem;
+
+namespace Hatfield.EnviroData.FileSystems.FTPFileSystem.Test
+{
+    [TestFixture]
+    public class FtpFileSystemTest
+    {
+        [Test]
+        [ExpectedException(typeof(UriFormatException))]
+        public void InvalidUriTest()
+        {
+            var uri = "invalid uri";
+            var httpFileSystem = new FTPFileSystem(uri);
+            var dataFromFileSystem = httpFileSystem.FetchData();
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NullUriTest()
+        {
+            var httpFileSystem = new FTPFileSystem(null);
+            httpFileSystem.FetchData();
+        }
+    }
+}
