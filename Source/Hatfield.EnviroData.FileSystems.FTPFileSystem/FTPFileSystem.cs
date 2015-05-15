5079c83a1d7270c9efa267858aa363a519b7c53b
Daniel <luzhanxun.daniel@gmail.com>
2015-05-15 14:18:25 -0700

    Update FTP file system
    Update FTP file system to support file filtering
    #3

--- Source/Hatfield.EnviroData.FileSystems.FTPFileSystem/FTPFileSystem.cs
+++ Source/Hatfield.EnviroData.FileSystems.FTPFileSystem/FTPFileSystem.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.IO;
 
 namespace Hatfield.EnviroData.FileSystems.FTPFileSystem
 {
@@ -11,19 +12,156 @@ namespace Hatfield.EnviroData.FileSystems.FTPFileSystem
         private Uri _uri;
         private NetworkCredential _credentials;
 
-        public FTPFileSystem(string url)
+        /// <summary>
+        /// Creates an anonymous FtpFileSystem object
+        /// </summary>
+        /// <param name="uri">A valid ftp uri (e.g. ftp://host:port/path/to/file/or/directory)</param>
+        /// <exception cref="ArgumentException">URI scheme is not FTP</exception>
+        public FTPFileSystem(string uri)
         {
-            _uri = new Uri(url);
-            _credentials = null;
+            Uri uri_ = new Uri(uri);
+
+            if (uri_.Scheme != Uri.UriSchemeFtp)
+            {
+                throw new ArgumentException(uri_ + "is not a valid FTP address");
+            }
+            else
+            {
+                _uri = uri_;
+                _credentials = null;
+            }
         }
 
-        public FTPFileSystem(string url, string username, string password)
+        /// <summary>
+        /// Creates a secure FtpFileSystem object
+        /// </summary>
+        /// <param name="uri">A valid ftp uri (e.g. ftp://host:port/path/to/file/or/directory)</param>
+        /// <param name="username">The username</param>
+        /// <param name="password">The password</param>
+        /// <exception cref="ArgumentException">URI scheme is not FTP</exception>
+        public FTPFileSystem(string uri, string username, string password)
         {
-            _uri = new Uri(url);
-            _credentials = new NetworkCredential(username, password);
+            Uri uri_ = new Uri(uri);
+
+            if (uri_.Scheme != Uri.UriSchemeFtp)
+            {
+                throw new ArgumentException(uri_ + "is not a valid FTP address");
+            }
+            else
+            {
+                _uri = uri_;
+                _credentials = new NetworkCredential(username, password);
+            }
         }
 
+        /// <summary>
+        /// Fetches the file in the uri
+        /// </summary>
+        /// <returns>DataFromFileSystem object of the file</returns>
+        /// <exception cref="ArgumentException">Uri is not a file (assume all files have extensions)</exception>
         public DataFromFileSystem FetchData()
+        {
+            // Detect whether its a directory or file
+            if (IsDirectory(_uri.LocalPath))
+            {
+                throw new ArgumentException(_uri + " is not a valid file path");
+            }
+            else
+            {
+                var fileStream = GetStream(_uri);
+                var fileName = Path.GetFileName(_uri.LocalPath);
+
+                return new DataFromFileSystem(fileName, fileStream);
+            }
+        }
+
+        /// <summary>
+        /// Fetches files that satisfies the filters from the directory
+        /// </summary>
+        /// <param name="filters">The desired filters</param>
+        /// <returns>List of DataFromFileSystem objects that satisfy the filters</returns>
+        /// <exception cref="ArgumentOutOfRangeException">Uri is not a directory (assume all directories have no extension)</exception>
+        public IEnumerable<DataFromFileSystem> FetchData(IEnumerable<IFileSystemFilter> filters)
+        {
+            // Detect whether its a directory or file
+            if (IsDirectory(_uri.LocalPath))
+            {
+                IEnumerable<string> filteredFileList = GetFilteredFileList(filters);
+                List<DataFromFileSystem> results = new List<DataFromFileSystem>();
+
+                foreach (string fileName in filteredFileList)
+                {
+                    Uri fullUri = new Uri(_uri, fileName);
+                    Stream stream = GetStream(fullUri);
+                    results.Add(new DataFromFileSystem(fileName, stream));
+                }
+
+                return results;
+            }
+            else
+            {
+                throw new ArgumentException(_uri + " is not a valid directory");
+            }
+        }
+
+        /// <summary>
+        /// Checks if a given uri is a directory
+        /// We assume that a path with no extension is a directory
+        /// </summary>
+        /// <param name="uri">The uri</param>
+        /// <returns>true if given uri is a directory, false otherwise</returns>
+        /// <exception cref="ArgumentNullException">Uri is null</exception>
+        private bool IsDirectory(string uri)
+        {
+            if (uri == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            return Path.GetExtension(uri) == string.Empty;
+        }
+
+        /// <summary>
+        /// Creates a list of file names in the ftp directory that satisfy the filters
+        /// </summary>
+        /// <param name="filters">The desired filters</param>
+        /// <returns>
+        /// List of file names satisfying the filters
+        /// e.g.: { directory/file1.ext, directory/file2.ext }
+        /// </returns>
+        private IEnumerable<string> GetFilteredFileList(IEnumerable<IFileSystemFilter> filters)
+        {
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(_uri);
+            request.Method = WebRequestMethods.Ftp.ListDirectory;
+
+            if (_credentials != null)
+            {
+                request.Credentials = _credentials;
+            }
+
+            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+            Stream responseStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(responseStream);
+            List<string> FilteredFileList = new List<string>();
+
+            while (!reader.EndOfStream)
+            {
+                string fileName = reader.ReadLine();
+                FilteredFileList.Add(fileName);
+            }
+
+            reader.Close();
+            response.Close();
+
+            return new List<string>(FilteredFileList);
+        }
+
+        /// <summary>
+        /// Gets a stream from the given ftp uri
+        /// </summary>
+        /// <param name="uri">The desired uri</param>
+        /// <returns>Stream from the given ftp uri</returns>
+        private Stream GetStream(Uri uri)
         {
             WebClient request = new WebClient();
 
@@ -32,16 +170,7 @@ namespace Hatfield.EnviroData.FileSystems.FTPFileSystem
                 request.Credentials = _credentials;
             }
 
-            var fileStream = request.OpenRead(_uri);
-            var fileName = System.IO.Path.GetFileName(_uri.LocalPath);
-
-            return new DataFromFileSystem(fileName, fileStream);
-        }
-
-
-        public IEnumerable<DataFromFileSystem> FetchData(IEnumerable<IFileSystemFilter> filters)
-        {
-            throw new NotImplementedException();
+            return request.OpenRead(uri);
         }
     }
 }
--- Test/Hatfield.EnviroData.FileSystems.FTPSystem.Test/FtpFileSystemTest.cs
+++ Test/Hatfield.EnviroData.FileSystems.FTPSystem.Test/FtpFileSystemTest.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+
 using NUnit.Framework;
 
 using Hatfield.EnviroData.FileSystems.FTPFileSystem;
@@ -12,20 +13,55 @@ namespace Hatfield.EnviroData.FileSystems.FTPFileSystem.Test
     public class FtpFileSystemTest
     {
         [Test]
-        [ExpectedException(typeof(UriFormatException))]
-        public void InvalidUriTest()
+        [ExpectedException(typeof(ArgumentException))]
+        [TestCase("http://httpsite.com")]
+        [TestCase("https://httpssite.com")]
+        public void FtpFileSystemAnonymousNotFtpTest(string uri)
         {
-            var uri = "invalid uri";
-            var httpFileSystem = new FTPFileSystem(uri);
-            var dataFromFileSystem = httpFileSystem.FetchData();
+            var ftpFileSystem = new FTPFileSystem(uri);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        [TestCase("http://httpsite.com")]
+        [TestCase("https://httpssite.com")]
+        public void FtpFileSystemSecureNotFtpTest(string uri)
+        {
+            var ftpFileSystem = new FTPFileSystem(uri, "username", "password");
+        }
+
+        [Test]
+        [ExpectedException(typeof(UriFormatException))]
+        [TestCase("not a valid uri")]
+        [TestCase("www.host.com")]
+        [TestCase("host.com")]
+        public void AnonymousFetchDataInvalidUriTest(string uri)
+        {
+            var ftpFileSystem = new FTPFileSystem(uri);
+        }
+
+        [Test]
+        [ExpectedException(typeof(UriFormatException))]
+        [TestCase("not a valid uri")]
+        [TestCase("www.host.com")]
+        [TestCase("host.com")]
+        public void SecureFetchDataInvalidUriTest(string uri)
+        {
+            var ftpFileSystem = new FTPFileSystem(uri, "username", "password");
         }
 
         [Test]
         [ExpectedException(typeof(ArgumentNullException))]
-        public void NullUriTest()
+        public void AnonymousFetchDataNullUriTest()
         {
-            var httpFileSystem = new FTPFileSystem(null);
-            httpFileSystem.FetchData();
+            var ftpFileSystem = new FTPFileSystem(null);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SecureFetchDataNullUriTest()
+        {
+            var ftpFileSystem = new FTPFileSystem(null, "username", "password");
         }
     }
 }
